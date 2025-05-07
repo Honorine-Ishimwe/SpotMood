ba41825085473076803543d7b3d7d5d80692008f
sanobertin <sanomihigobertin@gmail.com>
2025-05-07 12:47:43 -0400

    minor fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/src/background.css"
 import { useEffect } from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => { // background.js dynamic import
@@ -12,8 +13,8 @@ export default function App({ Component, pageProps }) {
 
   return (
       <>
-          <Script src="https://requirejs.org/docs/release/2.1.15/minified/require.js" strategy="afterInteractive" async />
-          <Script src="https://rawgit.com/ironwallaby/delaunay/master/delaunay.js" strategy="afterInteractive" async />
+          <Script src="https://requirejs.org/docs/release/2.1.15/minified/require.js" strategy="afterInteractive"  />
+          <Script src="https://rawgit.com/ironwallaby/delaunay/master/delaunay.js" strategy="afterInteractive"  />
 
           <canvas id="stars" width="100%" height="100%"></canvas>
           <Component {...pageProps} />
